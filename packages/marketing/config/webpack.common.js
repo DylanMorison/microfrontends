4464cf137d30416c30668081a2d1c75e5fcea2a1
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2022-04-11 16:18:09 -0700

    marketing webpack configs

---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,18 @@
+module.exports = {
+	module: {
+		rules: [
+			// loader tells webpack to process files as we load them into our project
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-react", "@babel/preset-env"],
+						plugins: ["@babel/plugin-transform-runtime"]
+					}
+				}
+			}
+		]
+	}
+};
