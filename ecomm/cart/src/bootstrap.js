72be88665c500d22ef3bedfc2ec343eec20e4417
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2022-04-11 14:57:05 -0700

    adds `mount` func to `cart`

--- ecomm/cart/src/bootstrap.js
+++ ecomm/cart/src/bootstrap.js
@@ -1,6 +1,20 @@
-
 import faker from "faker";
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+	const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector("#cart-dev").innerHTML = cartText;
+	el.innerHTML = cartText;
+};
+
+if (process.env.NODE_ENV === "development") {
+	const el = document.querySelector("#cart-dev");
+
+	// Assuming our container doesnt have an element
+	// with id `dev-products`
+	if (el) {
+		// We are probably running in isolation
+		mount(el);
+	}
+}
+
+export { mount };
--- ecomm/container/src/bootstrap.js
+++ ecomm/container/src/bootstrap.js
@@ -1,9 +1,10 @@
 // if webpack cannot find the "products" directory in node_modules
 // it will look in the `remotes` property of the ModuleFederationPlugin, and check
 // if the key of `products` exists.
-import { mount } from "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as productsMount} from "products/ProductsIndex";
+import { mount as cartMount } from "cart/CartShow";
 
 console.log("Container!");
 
-mount(document.querySelector("#my-products"));
+productsMount(document.querySelector("#my-products"));
+cartMount(document.querySelector("#my-cart"));
