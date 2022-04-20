a3297cb1f85aa0cd1f6d4a0933e7f9edfa5a1eb7
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2022-04-19 17:16:40 -0700

    container prod webpack

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require("webpack-merge");
 // takes an html file inside our project and injects some script tags inside of it
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
@@ -21,9 +20,6 @@ const devConfig = {
 				marketing: "marketing@http://localhost:8081/remoteEntry.js"
 			},
 			shared: packageJson.dependencies
-		}),
-		new HtmlWebpackPlugin({
-			template: "./public/index.html"
 		})
 	]
 };
---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,30 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+// we will set up this environment variable in the cicd pipeline
+// will contain string that tells us where our production application is hosted
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+	// minifies & optimizes all js files when `production` mode is set
+	mode: "production",
+	output: {
+		// ensure whenever we build files for prod, we will use `[name].[contenthash].js` as a template
+		filename: "[name].[contenthash].js"
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			// not required for a host module
+			name: "container",
+			remotes: {
+				// we are going to assume that remoteEntry.js is going to be nested inside a folder
+				marketing: `marketing@${domain}/marketing/remoteEntry.js`
+			},
+			shared: packageJson.dependencies
+		})
+	]
+};
+
+module.exports = merge(commonConfig, prodConfig);
