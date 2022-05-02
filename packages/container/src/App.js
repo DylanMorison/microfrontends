17070eb3b80ab2fa557ec5b04471097688f46887
DylanMorison <dylan@g.morison.org>
2022-05-02 13:41:44 -0700

    rebase

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,24 +1,26 @@
-import { createGenerateClassName, StylesProvider } from "@material-ui/core";
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header";
-import MarketingApp from "./components/MarketingApp";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 const generateClassName = createGenerateClassName({
-	productionPrefix: "co"
+  productionPrefix: 'co',
 });
 
-const App = () => {
-	return (
-		<>
-			<BrowserRouter>
-				<StylesProvider generateClassName={generateClassName}>
-					<Header />
-					<MarketingApp />
-				</StylesProvider>
-			</BrowserRouter>
-		</>
-	);
+export default () => {
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
 };
-
-export default App;
--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -1,6 +1,5 @@
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-// we dont need to check for dev because Container will ALWAYS show all other microservices
-ReactDom.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
