ac6148116855b8a4c97d4ebac1f5e97224ff672e
DylanMorison <dylan@g.morison.org>
2022-05-02 15:41:40 -0700

    adds auth workflow

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import { mount } from "auth/AuthApp";
 import React, { useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -14,9 +14,11 @@ export default () => {
         history.push(nextPathname);
       }
     };
+
     const { onParentNavigate } = mount(ref.current, {
       initialPath: history.location.pathname,
       onNavigate,
+      onSignIn,
     });
 
     history.listen(onParentNavigate);
