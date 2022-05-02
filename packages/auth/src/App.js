5f503bf60862c6e723cbe77b8cf55132421f71b7
DylanMorison <dylan@g.morison.org>
2022-05-02 13:52:00 -0700

    add auth components

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { Switch, Route, Router } from 'react-router-dom';
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
 import {
-  StylesProvider,
-  createGenerateClassName,
-} from '@material-ui/core/styles';
-
-// import Landing from './components/Landing';
-// import Pricing from './components/Pricing';
+	StylesProvider,
+	createGenerateClassName
+} from "@material-ui/core/styles";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'au',
+	productionPrefix: "au"
 });
 
 export default ({ history }) => {
-  return (
-    <div>
-      <StylesProvider generateClassName={generateClassName}>
-        <Router history={history}>
-          <Switch></Switch>
-        </Router>
-      </StylesProvider>
-    </div>
-  );
+	return (
+		<div>
+			<StylesProvider generateClassName={generateClassName}>
+				<Router history={history}>
+					<Switch>
+						<Route path="/auth/signin" component={SignIn} />
+						<Route path="/auth/signup" component={SignUp} />
+					</Switch>
+				</Router>
+			</StylesProvider>
+		</div>
+	);
 };
