956b363e1c94f250cb24a7d9f04311f88e3378ee
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2022-05-02 11:25:04 -0700

    reinit packages

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,32 +1,30 @@
-const { merge } = require("webpack-merge");
-// takes an html file inside our project and injects some script tags inside of it
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const commonConfig = require("./webpack.common");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const packageJson = require("../package.json")
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
-// configuration to give webpack when we are in dev mode
 const devConfig = {
-	mode: "development",
-	devServer: {
-		port: 8081,
-		historyApiFallback: {
-			index: "index.html"
-		}
-	},
-	plugins: [
-		new ModuleFederationPlugin({
-			name: "marketing",
-			filename: "remoteEntry.js",
-			exposes: {
-				"./MarketingApp": "./src/bootstrap"
-			},
-			shared: packageJson.dependencies
-		}),
-		new HtmlWebpackPlugin({
-			template: "./public/index.html"
-		})
-	]
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
 
 module.exports = merge(commonConfig, devConfig);
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,29 +1,24 @@
-const { merge } = require("webpack-merge");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require("./webpack.common");
-const packageJson = require("../package.json");
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
 
 const prodConfig = {
-	// minifies & optimizes all js files when `production` mode is set
-	mode: "production",
-	output: {
-		// ensure whenever we build files for prod, we will use `[name].[contenthash].js` as a template
-		filename: "[name].[contenthash].js",
-		// we set this so different script tags inside our html file will refer to the correct location of all of our javascript files
-		// in this case, we are setting the remotePath so ModuleFederationPlugin knows where to get the remoteEntry.js file
-		publicPath: "/marketing/latest/"
-	},
-	plugins: [
-		new ModuleFederationPlugin({
-			// not required for a host module
-			name: "marketing",
-			filename: "remoteEntry.js",
-			exposes: {
-				"./MarketingApp": "./src/bootstrap"
-			},
-			shared: packageJson.dependencies
-		})
-	]
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
 };
 
 module.exports = merge(commonConfig, prodConfig);
